934155cb7e76d253d9f0e3859d889c98de9cc514
RayhanalKavey <rayhanalkavey@gmail.com>
2022-12-29 16:41:53 +0600

    Add task and get tasks from the database.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5005;
 
+const tasks = require("./task.json");
+
 //Middle ware
 app.use(cors());
 app.use(express.json());
@@ -26,10 +28,28 @@ async function run() {
     const userCollection = client.db("taskController").collection("users");
     //Task Collection --2
     const taskCollection = client.db("taskController").collection("tasks");
+
+    //--2 Get all task for adding tasks
+    app.get(`/tasks`, async (req, res) => {
+      const query = {};
+      const tasks = await taskCollection.find(query).toArray();
+      res.send(tasks);
+    });
+
+    //--2 Post task for adding tasks
+    app.post("/tasks", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
   } finally {
   }
 }
+run().catch(console.dir);
 //-------------------------------
+// app.get("/tasks", (req, res) => {
+//   res.send(tasks);
+// });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Task Controller server");
